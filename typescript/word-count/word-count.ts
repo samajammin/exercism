080833a748ae5bc9b4a38c3d1af5b76823b07b76
Sam Richards <sbrichards@gmail.com>
2018-10-11 15:55:19 -0700

    Refactor word-count.ts

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -3,16 +3,11 @@ export default class Words {
     const words = phrase
       .trim()
       .split(/\s+/)
-      // .filter(word => word)
       .map(word => word.toLowerCase());
     let counts = new Map<string, number>();
     words.forEach(word => {
-      const wordCount = counts.get(word);
-      if (wordCount) {
-        counts.set(word, wordCount + 1);
-      } else {
-        counts.set(word, 1);
-      }
+      const wordCount = counts.get(word) || 0;
+      counts.set(word, wordCount + 1);
     });
     return counts;
   }

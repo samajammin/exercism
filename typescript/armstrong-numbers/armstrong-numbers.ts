efaeff3a74e0cba538abc6f34f0ae2abdd385cb8
Sam Richards <sbrichards@gmail.com>
2019-01-07 10:48:38 -0800

    Lint armstrong-numbers.ts

--- typescript/armstrong-numbers/armstrong-numbers.ts
+++ typescript/armstrong-numbers/armstrong-numbers.ts
@@ -1,12 +1,11 @@
 export default class ArmstrongNumbers {
   static isArmstrongNumber(num: number): boolean {
-    const numDigits = num.toString().length;
     const digitArr = num
       .toString()
       .split('')
       .map(str => Number(str));
     const result = digitArr.reduce((result, digit) => {
-      return result + digit ** numDigits;
+      return result + digit ** digitArr.length;
     }, 0);
 
     return result === num;

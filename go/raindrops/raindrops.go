35bd431599ec8df9dfc842c7cf81524317d0a99f
Sam Richards <sbrichards@gmail.com>
2019-05-03 16:45:16 -0700

    Solve raindrops.go

---
+++ go/raindrops/raindrops.go
@@ -0,0 +1,21 @@
+package raindrops
+
+import "strconv"
+
+// Convert converts a number to string depending on its factors
+func Convert(num int) string {
+	str := ""
+	if num%3 == 0 {
+		str += "Pling"
+	}
+	if num%5 == 0 {
+		str += "Plang"
+	}
+	if num%7 == 0 {
+		str += "Plong"
+	}
+	if len(str) == 0 {
+		return strconv.Itoa(num)
+	}
+	return str
+}

b6ebff03078fa0735f3093cbd2e8991d10417ec4
Sam Richards <sbrichards@gmail.com>
2019-05-09 12:03:39 -0700

    Refactor proverb.go

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -2,20 +2,19 @@ package proverb
 
 import "fmt"
 
-// Proverb creates a proverb from an array of words
+// Proverb constructs a proverb from an array of words
 func Proverb(rhyme []string) []string {
-	if len(rhyme) == 0 {
-		return rhyme
+
+	length := len(rhyme)
+	proverb := make([]string, length)
+
+	for i := 0; i < length; i++ {
+		if i == length-1 {
+			proverb[i] = fmt.Sprintf("And all for the want of a %s.", rhyme[0])
+		} else {
+			proverb[i] = fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i], rhyme[i+1])
+		}
 	}
 
-	proverb := make([]string, 0)
-
-	for i := 0; i < len(rhyme)-1; i++ {
-		phrase := fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i], rhyme[i+1])
-		proverb = append(proverb, phrase)
-	}
-
-	finalPhrase := fmt.Sprintf("And all for the want of a %s.", rhyme[0])
-	proverb = append(proverb, finalPhrase)
 	return proverb
 }

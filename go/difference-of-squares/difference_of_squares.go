059827dbb9827f8c7327282d222cf22fdc3a67a6
Sam Richards <sbrichards@gmail.com>
2019-06-06 10:58:12 -0700

    Refactor difference_of_squares.go

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -2,18 +2,13 @@ package diffsquares
 
 // SquareOfSum calculates the square of the sum of positive ints up to num.
 func SquareOfSum(num int) (res int) {
-	for i := 1; i <= num; i++ {
-		res += i
-	}
-	return res * res
+	sum := (num * (num + 1)) / 2
+	return sum * sum
 }
 
 // SumOfSquares calculates the sum of positive ints squared up to num.
 func SumOfSquares(num int) (res int) {
-	for i := 1; i <= num; i++ {
-		res += i * i
-	}
-	return res
+	return num * (num + 1) * (2*num + 1) / 6
 }
 
 // Difference calculates the difference between SquareOfSum and SumOfSquares.

9c444e2b459e2ab95979982655d665d135437622
Sam Richards <sbrichards@gmail.com>
2019-06-12 10:45:34 -0700

    Refactor sublist.ts

--- typescript/sublist/sublist.ts
+++ typescript/sublist/sublist.ts
@@ -6,40 +6,38 @@ enum ListComparisons {
 }
 
 export default class List {
-  private readonly _list: number[]
+  private readonly _values: number[]
 
   constructor(...args: number[]) {
-    this._list = args
+    this._values = args
   }
 
-  public compare(list: List): string {
-    let superlist = this._list
-    let sublist = list._list
-    const isShorter = this._list.length < list._list.length
+  public compare(that: List): string {
+    let [longList, shortList] = [this._values, that._values]
+    const isShorter = longList.length < shortList.length
     if (isShorter) {
-      superlist = list._list
-      sublist = this._list
+      [longList, shortList] = [shortList, longList]
     }
 
     // empty lists are equal
-    if (superlist.length === 0) {
+    if (longList.length === 0) {
       return ListComparisons.Equal
     }
     // or empty lists are sublists
-    if (sublist[0] === undefined) {
+    if (shortList[0] === undefined) {
       return isShorter ? ListComparisons.Sublist : ListComparisons.Superlist
     }
 
-    for (let i = 0; i < superlist.length; i++) {
-      if (superlist[i] === sublist[0]) {
-        for (let j = 1; j < sublist.length; j++) {
-          if (superlist[i + j] !== sublist[j]) {
+    for (let i = 0; i < longList.length; i++) {
+      if (longList[i] === shortList[0]) {
+        for (let j = 1; j < shortList.length; j++) {
+          if (longList[i + j] !== shortList[j]) {
             break
           }
-          if (j === sublist.length - 1 && j === superlist.length - 1) {
+          if (j === shortList.length - 1 && j === longList.length - 1) {
             return ListComparisons.Equal
           }
-          if (j === sublist.length - 1) {
+          if (j === shortList.length - 1) {
             return isShorter
               ? ListComparisons.Sublist
               : ListComparisons.Superlist

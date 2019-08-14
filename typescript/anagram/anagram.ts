0137630503718828ad07cf3c5bb1a36e57f3d2f0
Sam Richards <sbrichards@gmail.com>
2019-08-14 11:22:44 -0700

    Refactor anagram.ts

--- typescript/anagram/anagram.ts
+++ typescript/anagram/anagram.ts
@@ -1,40 +1,15 @@
-type charCount = { [char: string]: number }
-
 export default class Anagram {
   private readonly anagram: string
-  private readonly charCounts: charCount
 
   constructor(anagram: string) {
     this.anagram = anagram.toLowerCase()
-    this.charCounts = this.getCharCounts(this.anagram)
-  }
-
-  private getCharCounts(word: string): charCount {
-    const charCounts: charCount = {}
-    for (const char of word) {
-      if (charCounts[char]) {
-        charCounts[char] += 1
-      } else {
-        charCounts[char] = 1
-      }
-    }
-    return charCounts
   }
 
   private isAnagram(word: string): boolean {
-    if (this.anagram.length !== word.length) {
-      return false
-    }
     if (this.anagram === word) {
       return false
     }
-    const wordCharCounts = this.getCharCounts(word)
-    for (const char in wordCharCounts) {
-      if (wordCharCounts[char] !== this.charCounts[char]) {
-        return false
-      }
-    }
-    return true
+    return [...this.anagram].sort().join('') === [...word].sort().join('')
   }
 
   public matches(...words: string[]): string[] {

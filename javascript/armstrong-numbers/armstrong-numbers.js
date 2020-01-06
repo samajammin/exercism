feb2b569da6f669083853acfcc51cfa462667cb0
Sam Richards <sbrichards@gmail.com>
2020-01-06 09:19:23 -0800

    Refactor armstrong-numbers.js

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -2,10 +2,10 @@ export const isArmstrongNumber = num => {
   if (typeof num !== "number") {
     throw new Error("Must supply a number.");
   }
-  const numString = num.toString();
-  let sum = 0;
-  for (const str of numString) {
-    sum += Number(str) ** numString.length;
-  }
-  return sum === num;
+  const numString = String(num);
+  const numDigits = numString.length;
+  return (
+    num ===
+    numString.split("").reduce((sum, str) => sum + Number(str) ** numDigits, 0)
+  );
 };
